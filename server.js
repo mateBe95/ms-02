f17fda219c05f9882101e8a161ef99f57b5414cb
Mateusz Bieda <mateusz.bieda95@gmail.com>
2025-10-03 13:36:26 +0200

    Trying to fix build

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import datasetsController from "./server/controllers/datasetsController.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Serwowanie statycznych plików frontendu
+app.use(express.static(path.join(__dirname, "build/client")));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client/dist/index.html"));
+// });
 
 app.use("/ui/api/datasets", datasetsController);
 
